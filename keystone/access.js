31046c82d005da834f0eabff089d1bdde0e44ea7
hughes <hughes@tumelo.com>
2020-05-10 12:50:04 +0100

    first commit

--- keystone/access.js
+++ keystone/access.js
@@ -1,14 +1,16 @@
 const userIsAdmin = ({ authentication: { item: user } }) => {
-  return !!user && user.isAdmin
-}
+  return !!user && user.isAdmin;
+};
+
 const userIsItem = ({ itemId, authentication: { item: user } }) => {
-  return !!user && user.id === itemId
+  return !!user && user.id === itemId;
   // return !!user && { id: user.id }
-}
-const userIsAdminOrItem = auth => userIsAdmin(auth) || userIsItem(auth)
+};
+
+const userIsAdminOrItem = (auth) => userIsAdmin(auth) || userIsItem(auth);
 
 module.exports = {
   userIsAdmin,
   userIsItem,
   userIsAdminOrItem,
-}
+};
